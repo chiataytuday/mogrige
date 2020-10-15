28be6817a88b589386696b65ad082d6461be619e
Taylor <hyobeen.moon@gmail.com>
2020-10-15 13:00:45 +0900

    SwipeApplied
    스와이프기능 화면 적용

---
+++ mogrige/mogrige/ViewController.swift
@@ -0,0 +1,141 @@
+//
+//  ViewController.swift
+//  mogrige
+//
+//  Created by Hyunseok Yang on 2020/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var keyword01: UILabel!
+    @IBOutlet weak var keyword02: UILabel!
+    @IBOutlet weak var keyword03: UILabel!
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBAction func resetButton(_ sender: Any) {
+        resetCard()
+    }
+    
+    @IBAction func closeModal(
+        _ segue: UIStoryboardSegue) {
+        
+    }
+    
+    var divisor: CGFloat!
+    
+    
+    //swipe animation 구현
+    @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
+        let card = sender.view!
+        let point = sender.translation(in: view)
+        let xFromCenter = card.center.x - view.center.x
+        let scale = min(100/abs(xFromCenter), 1)
+        
+        card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+        //카드 돌아가는 애니메이션
+        card.transform = CGAffineTransform(rotationAngle: xFromCenter/divisor).scaledBy(x: scale, y: scale)
+        
+        //카드를 원래 자리로 복귀
+        func cardFormatReset() {
+            card.center = self.view.center
+            thumbImageView.alpha = 0
+            card.transform = CGAffineTransform.identity
+        }
+        
+        
+        if xFromCenter > 0 {
+            thumbImageView.image = UIImage(named: "thumbsUp")
+        } else {
+            thumbImageView.image = UIImage(named: "thumbsDown")
+        }
+        
+        thumbImageView.alpha = abs(xFromCenter) / view.center.x
+        
+        //card fade away when it passes certain pointof x-axis
+        if sender.state == UIGestureRecognizer.State.ended {
+            
+            if card.center.x < 85 {
+                // move off to the left side
+                UIView.animate(withDuration: 0.3, animations: {
+                    card.center = CGPoint(x: card.center.x - 300, y: card.center.y + 61)
+                })
+                cardFormatReset()
+                randomPicked()
+                return
+            } else if card.center.x > (view.frame.width - 85) {
+                // move off to the right side
+                UIView.animate(withDuration: 0.3,animations: {
+                    card.center = CGPoint(x: card.center.x + 300, y: card.center.y + 61)
+                })
+                cardFormatReset()
+                return
+            } else {
+                cardFormatReset()
+            }
+        }
+    }
+    
+    //카드가 끝까지 넘어가지 않은 경우 원래 자리로 복귀
+    func animate() {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.cardView.center = self.view.center
+        })
+    }
+    
+    //temporary reset button
+    func resetCard() {
+        animate()
+        self.thumbImageView.alpha = 0
+        self.cardView.alpha = 1
+        self.cardView.transform = .identity
+    }
+    
+    // 랜덤키워드 만들기
+    func randomPicked() {
+        var resultSet = Set<String>()
+
+        while resultSet.count < 3 {
+            let randomIndext = Int.random(in: 0...keywordList.count-1)
+            resultSet.insert(keywordList[randomIndext])
+        }
+        let resultArray = Array(resultSet)
+        
+        keyword01.text = resultArray[0]
+        keyword02.text = resultArray[1]
+        keyword03.text = resultArray[2]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //카드 회전 애니메이션 추가
+        divisor = (view.frame.width / 2) / 0.61
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        randomPicked()
+    }
+
+    // 리픽 버튼 눌렀을때 랜덤픽하기 ---> swipe 적용으로 리픽버튼 OFF
+//    @IBAction func rePick(_ sender: Any) {
+//        randomPicked()
+//    }
+    
+    
+    // 모달에 데이터 전달하기
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        let pickedKeword = [keyword01.text, keyword02.text, keyword03.text]
+
+        guard let vc = segue.destination as? ModalViewController else { return }
+        
+        let selectedTitle:String! = "#\(pickedKeword[0]!),  #\(pickedKeword[1]!),  #\(pickedKeword[2]!)"
+
+        vc.selectedTitle = selectedTitle
+        
+    }
+
+
+}
+
